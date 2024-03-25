e53e010c9e0a91337111c309384fd79b965ca3ce
bharathielumalai5335 <159746627+bharathielumalai5335@users.noreply.github.com>
2024-03-25 12:57:44 +0530

    Update arithmetic op.java

--- arithmetic op.java
+++ arithmetic op.java
@@ -4,6 +4,6 @@ public class divideOfNumbers1.
 public static void main(String args[])
 {
 int n1 = 225, n2 = 115, div;
-div = n1 × n2;
-System.out.println("The divide of numbers is: "+sub);
+div = n1 / n2;
+System.out.println("The divide of numbers is: "+div);
 }
